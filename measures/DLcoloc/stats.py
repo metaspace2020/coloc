6618f4d8e6b4d328cc904fbcab714ec645f0e750
alexander-rakhlin <rakhlin@gmx.net>
2019-12-23 12:20:05 +0300

    Add conf_intervals3

--- measures/DLcoloc/stats.py
+++ measures/DLcoloc/stats.py
@@ -32,6 +32,25 @@ def conf_intervals(df, y_nm, y_pred_nm, fun, n=100):
     return conf
 
 
+def conf_intervals3(df, y_nm, y_pred_nm, fun, n=100):
+    corrs = []
+    for g in df.groupby(['datasetId', 'baseSf', 'baseAdduct']):
+        res = fun(g[1][y_nm], g[1][y_pred_nm])[0]
+        corrs.append(res)
+    corrs = np.array(corrs)
+
+    sample = np.array([corrs[np.random.choice(len(corrs), len(corrs), replace=True)].mean() for _ in range(n)])
+
+    ci_lo = np.percentile(sample, 2.5)
+    median = np.percentile(sample, 50)
+    mean = np.mean(sample)
+    st = np.std(sample)
+    ci_hi = np.percentile(sample, 97.5)
+
+    conf = f'mean={mean:0.3f}|median={median:0.3f}|std={st:0.3f} ({ci_lo:0.3f}-{ci_hi:0.3f}) (95% CI)'
+    return conf
+
+
 def accuracy_datasetwise(df, cols, fun=spearmanr):
     acc = []
     for g in df.groupby(['datasetId', 'baseSf', 'baseAdduct']):
@@ -50,5 +69,5 @@ if __name__ == '__main__':
     print(f'Kendalltau dataset-wise: mean {mean:0.3f}, median {median:0.3f}')
 
     print('_____________________\nConfidence intervals:')
-    print('Spearman:', conf_intervals(df_preds, 'rank', 'pred', spearmanr, n=100))
-    print('Kendalltau:', conf_intervals(df_preds, 'rank', 'pred', kendalltau, n=100))
+    print('Spearman:', conf_intervals3(df_preds, 'rank', 'pred', spearmanr))
+    print('Kendalltau:', conf_intervals3(df_preds, 'rank', 'pred', kendalltau))
